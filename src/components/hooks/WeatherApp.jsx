d10b11c3a2f17cde5638e866c85ebec58d0d5cd0
DiyorMr <dmuxiddinov594@gmail.com>
2025-08-09 11:25:46 +0500

    bugs done api

--- src/components/hooks/WeatherApp.jsx
+++ src/components/hooks/WeatherApp.jsx
@@ -19,7 +19,6 @@ const WeatherApp = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100 px-4">
             <h1 className="text-3xl font-bold text-blue-600 mb-4">Weather App</h1>
-
             <div className="flex gap-2">
                 <input
                     type="text"
@@ -32,10 +31,9 @@ const WeatherApp = () => {
                     onClick={getWeather}
                     className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
                 >
-                    Search
+                    Searching
                 </button>
             </div>
-
             {weather && weather.main && (
                 <div className="mt-6 bg-white p-6 rounded shadow text-center">
                     <h2 className="text-xl font-semibold text-gray-800">{weather.name}</h2>
